af76f3a8c62e752a90caade7043bce7c867d2db1
Your Name <iankibet87@gmail.com>
2023-07-09 02:49:56 +0800

    modifyind element

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1');
+
+  newHeader.id = 'victory';
+
+  newHeader.textContent = 'Ian is the champion';
+
+  document.body.appendChild(newHeader);
+
+console.log(document)
+
+
+
+ 
+ 
+  
+  
+  
+  
+  
+  
